9630dcd940a067ee6c0104f65fdc5543012037ec
Lucas Martins Arruda <lucas.martins@tigestao.com.br>
2023-01-13 10:03:56 -0300

    criação de pacote security e correção de pacote entities

--- src/main/java/com/example/demo/repositories/PersonRepository.java
+++ src/main/java/com/example/demo/repositories/PersonRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.demo.enitities.Person;
+
 
 @Repository
 public class PersonRepository extends JpaRepository<Person, String> {
